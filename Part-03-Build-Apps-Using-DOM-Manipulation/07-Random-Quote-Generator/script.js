e84ca6751b5716be174ac0d0fe905373dc115071
sidp919 <sidp0008@gmail.com>
2022-10-23 11:34:40 +0530

    Made changes in QuoteGenerator app as per requirements

--- Part-03-Build-Apps-Using-DOM-Manipulation/07-Random-Quote-Generator/script.js
+++ Part-03-Build-Apps-Using-DOM-Manipulation/07-Random-Quote-Generator/script.js
@@ -2,18 +2,25 @@ let quoteElement = document.querySelector("#quote");
 let authorElement = document.querySelector("#quote-author");
 let quoteBtn = document.querySelector("#generate");
 
-quoteBtn.addEventListener("click", async () => {
+// quoteBtn.addEventListener("click", );
+
+(
+    async () => {
     
-    let quote = await fetch("https://api.quotable.io/random?tags=famous-quotes&maxLength=48")
-    .then((rawdata) => rawdata.json())
-    .then((parsedData) => parsedData)
-    .catch((err)=>err)
-    .finally(() => {
-        console.log("Thanks to https://github.com/lukePeavey/quotable");
-    });
-
-    quoteElement.style.fontStyle = "italic";
-    quoteElement.innerText = quote.content;
-    authorElement.innerText = "- " + quote.author;
-
-});
+        let quote = await fetch("https://api.quotable.io/random?tags=famous-quotes&maxLength=48")
+        .then((rawdata) => rawdata.json())
+        .catch((err)=>err)
+        .finally(() => {
+            console.log("Thanks to https://github.com/lukePeavey/quotable");
+        });
+    
+        if(quote.content!=null){
+            quoteElement.style.fontStyle = "italic";
+            quoteElement.innerText = quote.content;
+            authorElement.innerText = "- " + quote.author;
+        }else {
+            quoteElement.innerText = "Something went wrong!\nPlease check your internet connection & Refresh the page."
+        }
+    
+    }
+)();
